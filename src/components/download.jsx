b2860cb6a8a96b6a49f558fe7a447e616d641854
Mohammed Ishaque <mohammedishaque@Ishaques-MacBook-Pro.local>
2021-12-06 14:49:50 +0530

    first commit

---
+++ src/components/download.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+
+const download = () => {
+ return ( 
+   <div id="download">
+<ul>
+<h1>Download from here !!!</h1>
+  <li>
+    <div className="download apple">
+      <i className="fa fa fa-apple fa-3x" />
+      <span className="df">Soon </span>
+      <span className="dfn">Mac </span>
+    </div>
+  </li>
+  <br />
+  <li>
+    <div className="download windows">
+      <i className="fa fa fa-windows fa-3x" />
+      <span className="df">Download for </span>
+      <span className="dfn">Windows</span>
+    </div>
+  </li>
+</ul>
+</div>
+ )
+}
+export default download;
---
+++ src/components/services.jsx
@@ -0,0 +1,29 @@
+import React, { Component }  from 'react';
+
+export const Services = (props) => {
+  return (
+    <div id='services' className='text-center'>
+      <div className='container'>
+        <div className='section-title'>
+          <h2>Benifits of AI Assistant</h2>
+          <p>
+          Voice assistants are powerful hands-free computing tools that gain new abilities almost every week          </p>
+        </div>
+        <div className='row'>
+          {props.data
+            ? props.data.map((d, i) => (
+                <div key={`${d.name}-${i}`} className='col-md-4'>
+                  {' '}
+                  <i className={d.icon}></i>
+                  <div className='service-desc'>
+                    <h3>{d.name}</h3>
+                    <p>{d.text}</p>
+                  </div>
+                </div>
+              ))
+            : 'loading'}
+        </div>
+      </div>
+    </div>
+  )
+}
